ad24c4a50ccc01f16620a24dda359b3817ed998c
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-23 16:48:47 -0800

    :sparkles: Add Spotify API fetching

--- api/spotify.ts
+++ api/spotify.ts
@@ -1,5 +1,45 @@
 import { NowRequest, NowResponse } from "@now/node";
+import SpotifyAPI from "spotify-web-api-node";
 
-export default (req: NowRequest, res: NowResponse) => {
-  res.json({ spotify: true });
+const spotify = new SpotifyAPI({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: "https://anandchowdhary.com/test-callback",
+  accessToken: process.env.SPOTIFY_ACCESS_TOKEN,
+  refreshToken: process.env.SPOTIFY_REFRESH_TOKEN
+});
+
+export default async (req: NowRequest, res: NowResponse) => {
+  try {
+    // const data = await spotify.refreshAccessToken();
+    // spotify.setAccessToken(data.body.access_token);
+    const longTermArtists = (
+      await spotify.getMyTopArtists({
+        time_range: "long_term"
+      })
+    ).body.items;
+    const mediumTermArtists = (
+      await spotify.getMyTopArtists({
+        time_range: "medium_term"
+      })
+    ).body.items;
+    const shortTermArtists = (
+      await spotify.getMyTopArtists({
+        time_range: "short_term"
+      })
+    ).body.items;
+    const spotifyArtists = JSON.stringify(
+      {
+        longTermArtists,
+        mediumTermArtists,
+        shortTermArtists
+      },
+      null,
+      2
+    );
+    res.json({ done: true });
+  } catch (error) {
+    res.status(500);
+    res.json({ error });
+  }
 };
