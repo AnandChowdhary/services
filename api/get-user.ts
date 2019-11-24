79df43f4348b2c1a4e960a6603dd42e61c1809fa
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-24 10:01:31 -0800

    :wrench: Add async to default service

--- api/get-user.ts
+++ api/get-user.ts
@@ -1,5 +1,10 @@
 import { NowRequest, NowResponse } from "@now/node";
 
-export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: "John", email: "john@example.com" });
+export default async (req: NowRequest, res: NowResponse) => {
+  try {
+    return res.json({ name: "John", email: "john@example.com" });
+  } catch (error) {
+    res.status(500);
+    res.json({ error });
+  }
 };
