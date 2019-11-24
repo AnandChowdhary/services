553d0b4bb28cdb77e0106f310d37fc38a42d79a6
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-24 10:27:18 -0800

    :sparkles: Add OpenStreetMaps helper

--- api/owntracks.ts
+++ api/owntracks.ts
@@ -1,7 +1,53 @@
 import { NowRequest, NowResponse } from "@now/node";
+import { reverseGeocoding } from "../helpers/open-street-maps";
+
+interface OwnTracks {
+  cog: number;
+  batt: number;
+  lon: number;
+  acc: number;
+  vel: number;
+  vac: number;
+  bs: number;
+  lat: number;
+  topic: string;
+  t: string;
+  conn: string;
+  tst: number;
+  alt: number;
+}
+
+const statusify = (status: number) => {
+  if (status === 1) return "unplugged";
+  if (status === 2) return "charging";
+  if (status === 3) return "full";
+  return "unknown";
+};
+
+const connectify = (status: string) => {
+  if (status === "w") return "wireless";
+  if (status === "o") return "offline";
+  if (status === "m") return "mobile";
+  return "unknown";
+};
 
 export default async (req: NowRequest, res: NowResponse) => {
   try {
+    const ownTracks: OwnTracks = req.body;
+    const data = {
+      accuracy: ownTracks.acc,
+      altitude: ownTracks.alt,
+      altitudeAccuracy: ownTracks.vac,
+      battery: ownTracks.batt,
+      batteryStatus: statusify(ownTracks.bs || 0),
+      latitude: ownTracks.lat,
+      longitude: ownTracks.lon,
+      velocity: ownTracks.vel,
+      device: ownTracks.topic,
+      connection: connectify(ownTracks.conn || "u"),
+      time: new Date(ownTracks.tst * 1000),
+      ...(await reverseGeocoding(ownTracks.lat, ownTracks.lon))
+    };
     console.log("Got ownTracks", JSON.stringify(req.body, null, 2));
     return res.json({ success: true });
   } catch (error) {
---
+++ helpers/open-street-maps.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from "axios";
+
+interface NominatimResult {
+  place_id: number;
+  licence: string;
+  lat: number;
+  lon: number;
+  display_name: string;
+  address: {
+    road: string;
+    locality: string;
+    state_district: string;
+    state: string;
+    postcode: string;
+    country: string;
+    country_code: string;
+  };
+}
+
+export const reverseGeocoding = async (latitude: number, longitude: number) => {
+  const result: AxiosResponse<NominatimResult> = await axios.get(
+    `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
+  );
+  const data = { ...result.data };
+  delete data.lat;
+  delete data.lon;
+  delete data.licence;
+  return data;
+};
