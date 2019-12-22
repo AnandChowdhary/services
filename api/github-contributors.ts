7f2ad447d8b4ca4644920f5eeff3e981d5cc6974
Anand Chowdhary <mail@anandchowdhary.com>
2019-12-22 00:13:48 -0500

    :sparkles: Add contributors API

--- api/github-contributors.ts
+++ api/github-contributors.ts
@@ -14,11 +14,23 @@ export default async (req: NowRequest, res: NowResponse) => {
       }
     )) as AxiosResponse<User[]>;
     const contributors = response.data.filter(user => user.type !== "Bot");
-    // res.setHeader(
-    //   "Cache-Control",
-    //   `max-age=${req.query.cacheAge || 86400}, public`
-    // );
-    return res.json({ contributors });
+    res.setHeader(
+      "Cache-Control",
+      `max-age=${req.query.cacheAge || 86400}, public`
+    );
+    res.setHeader("Content-Type", "image/svg+xml");
+    return res.send(`
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        ${contributors
+          .map((contributor, i) => {
+            return `<image x="${(i % 8) * 90 + 5}" y="${Math.floor(i / 8) * 90 +
+              5}" width="85" height="85" xlink:href="${
+              contributor.avatar_url
+            }"><title>${contributor.login}</title></image>`;
+          })
+          .join("")}
+      </svg>
+    `);
   } catch (error) {
     return res.status(500).json({ error });
   }
