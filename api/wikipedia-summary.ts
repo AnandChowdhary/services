7109e9a7f25e366958322fa282554a1ae75974ce
Anand Chowdhary <mail@anandchowdhary.com>
2019-12-26 11:34:42 -0500

    :sparkles: Add Wikipedia summary endpoint

---
+++ api/wikipedia-summary.ts
@@ -0,0 +1,51 @@
+import { NowRequest, NowResponse } from "@now/node";
+import axios from "axios";
+import natural from "natural";
+const SentenceTokenizer = new (natural as any).SentenceTokenizer() as natural.WordTokenizer;
+
+const safeParam = (param: string | string[], value: number) =>
+  param && typeof param === "string" && !isNaN(parseInt(param))
+    ? parseInt(param)
+    : value;
+
+export default async (req: NowRequest, res: NowResponse) => {
+  try {
+    const searchResults = (
+      await axios.get<{
+        query: { search: { title: string; pageid: string }[] };
+      }>(
+        `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${encodeURIComponent(
+          req.query.q as string
+        )}`
+      )
+    ).data;
+    const pageId = searchResults.query.search[0].pageid;
+    const pageResult = (
+      await axios.get<{
+        query: {
+          pages: { [index: string]: { title: string; extract: string } };
+        };
+      }>(
+        `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${pageId}`
+      )
+    ).data;
+    const limit = safeParam(req.query.length, 300);
+    const min = safeParam(req.query.length, 50);
+    const tokens = SentenceTokenizer.tokenize(
+      pageResult.query.pages[pageId].extract
+    );
+    let result = "";
+    let length = 0;
+    tokens.forEach(token => {
+      if (length + token.length <= limit) {
+        result += ` ${token}`;
+        length += token.length;
+      }
+    });
+    if (result.length < min) throw new Error("too short");
+    res.send(result);
+  } catch (error) {
+    res.status(404);
+    res.json({ error: "not found" });
+  }
+};
