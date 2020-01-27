974697747a4ee84d55db114b7d8b5bf9fd2a872b
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-27 19:55:04 +0530

    :sparkles: Add podcasts endpoints

--- api/pocket-casts.ts
+++ api/pocket-casts.ts
@@ -1,8 +1,31 @@
 import { NowRequest, NowResponse } from "@now/node";
+import { safeDump } from "js-yaml";
+import PocketCasts from "pocketcasts";
+import { writeGitHubFile } from "../helpers/github";
+
+const pocketCasts = new PocketCasts(
+  process.env.POCKET_CASTS_USERNAME,
+  process.env.POCKET_CASTS_PASSWORD
+);
 
 export default async (req: NowRequest, res: NowResponse) => {
   try {
-    return res.json({ name: "John", email: "john@example.com" });
+    await pocketCasts.login();
+    const list = safeDump(await pocketCasts.getList());
+    const history = safeDump(await pocketCasts.getHistory());
+    await writeGitHubFile(
+      "AnandChowdhary/life-data",
+      "podcasts.yml",
+      "🎙️ Update Pocket Casts list",
+      list
+    );
+    await writeGitHubFile(
+      "AnandChowdhary/life-data",
+      "podcast-history.yml",
+      "🎤 Update Pocket Cast history",
+      history
+    );
+    res.json({ done: true });
   } catch (error) {
     res.status(500);
     res.json({ error });
