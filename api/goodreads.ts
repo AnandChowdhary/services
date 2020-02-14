ce73aecd537432c2b96762f5dde03b41a479c321
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-14 14:17:48 +0100

    :heavy_plus_sign: Add xml-js

--- api/goodreads.ts
+++ api/goodreads.ts
@@ -1,12 +1,18 @@
 import { NowRequest, NowResponse } from "@now/node";
-import goodreads from "goodreads";
-const gr = new goodreads.client({});
+import axios from "axios";
 
 export default async (req: NowRequest, res: NowResponse) => {
   try {
-    return res.json({ name: "John", email: "john@example.com" });
+    const data = await axios.get(
+      `https://www.goodreads.com/review/list/6784560.xml?shelf=read&key=${process.env.GOODREADS_KEY}&v=2`,
+      {
+        responseType: "text"
+      }
+    );
+    res.json({ user: "data" });
   } catch (error) {
     res.status(500);
+    console.log(error);
     res.json({ error });
   }
 };
